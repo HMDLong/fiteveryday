6f8695aa4c27b6c6f806939eabc7d64ad66de0d5
HMDLong <long.hmd2000@gmail.com>
2024-08-05 23:50:52 +0700

    login page UI

--- mobile/fiteveryday/lib/features/auth/screen/login_page.dart
+++ mobile/fiteveryday/lib/features/auth/screen/login_page.dart
@@ -12,19 +12,61 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Text("login"),
-            ElevatedButton(
-              onPressed: () {
-                context.go("/home");
-              },
-              child: Text("to home"),
-            )
-          ],
+      body: SizedBox.expand(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Login"),
+              const SizedBox(height: 6),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(18),
+                    borderSide: BorderSide(width: 0.5),
+                  ),
+                  label: Text("Username"),
+                  labelStyle: TextStyle(fontSize: 12),
+                ),
+                onChanged: (value) {},
+              ),
+              const SizedBox(height: 18),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(18),
+                    borderSide: BorderSide(width: 0.5),
+                  ),
+                  label: Text("Password"),
+                  labelStyle: TextStyle(fontSize: 12),
+                ),
+                onChanged: (value) {},
+              ),
+              const SizedBox(height: 36),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: onLogin,
+                  child: Text("Đăng nhập"),
+                ),
+              ),
+              const SizedBox(height: 6),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: onLogin,
+                  child: Text("Đăng nhập với Google"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> onLogin() async {
+    context.go("/home");
   }
 }
